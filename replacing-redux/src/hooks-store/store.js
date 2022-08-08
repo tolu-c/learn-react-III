991d82d7457a2f56d8162016d8348df7a0a9cf1f
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-08-08 16:41:45 +0000

    custom hook store

--- replacing-redux/src/hooks-store/store.js
+++ replacing-redux/src/hooks-store/store.js
@@ -7,11 +7,29 @@ let actions = {};
 export const useStore = () => {
   const setState = useState(globalState)[1];
 
+  const dispatch = (actionIdentifier) => {
+    const newState = actions[actionIdentifier](globalState);
+    globalState = { ...globalState, ...newState };
+
+    for (const listener of listeners) {
+      listener(globalState);
+    }
+  };
+
   useEffect(() => {
     listeners.push(setState);
 
     return () => {
       listeners = listeners.filter((listener) => listener !== setState);
-    }
+    };
   }, [setState]);
+
+  return [globalState, dispatch];
+};
+
+export const initStore = (userActions, initialState) => {
+  if (initialState) {
+    globalState = { ...globalState, ...initialState };
+  }
+  actions = { ...actions, ...userActions };
 };
