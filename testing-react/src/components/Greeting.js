17ce5c6ff4a55a22eb59324301c79299245a63f6
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-08-10 07:43:58 +0000

    writing my first test

---
+++ testing-react/src/components/Greeting.js
@@ -0,0 +1,10 @@
+const Greeting = () => {
+  return (
+    <div>
+      <h2>Hello world!</h2>
+      <p>it's good to see you.</p>
+    </div>
+  );
+};
+
+export default Greeting;
---
+++ testing-react/src/components/Greeting.test.js
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import Greeting from "./Greeting";
+
+test("checks for the hello world", () => {
+  // arrange
+  render(<Greeting />);
+  // acts
+  // => noting to do
+  // asserts
+  const check = screen.getByText(/Hello world/, { exact: false });
+  expect(check).toBeInTheDocument();
+});
