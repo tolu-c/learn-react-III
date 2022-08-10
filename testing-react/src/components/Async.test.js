acbd8d3aa8dc4bb236a3f5a8161f789a21b66e51
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-08-10 10:46:37 +0000

    working with mocks

--- testing-react/src/components/Async.test.js
+++ testing-react/src/components/Async.test.js
@@ -3,6 +3,15 @@ import Async from "./Async";
 
 describe("Async component", () => {
   test("checks if post renders correctly", async () => {
+    window.fetch = jest.fn()
+    window.fetch.mockResolvedValue({
+      json: async () => ([{ title: "post 1" }, { title: "post 2" }])
+    });
+    // .mockImplementation(() => {
+    //   return Promise.resolve({
+    //     json: () => Promise.resolve([{ title: "post 1" }, { title: "post 2" }]),
+    //   });
+    // });
     render(<Async />);
 
     const check = await screen.findAllByRole("listitem");
