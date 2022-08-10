93d7d79da7b7cada9c09cfaded5454391083442a
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-08-10 08:22:06 +0000

    grouping tests to test suites

--- testing-react/src/components/Greeting.test.js
+++ testing-react/src/components/Greeting.test.js
@@ -1,12 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting";
 
-test("checks for the hello world", () => {
-  // arrange
-  render(<Greeting />);
-  // acts
-  // => noting to do
-  // asserts
-  const check = screen.getByText(/Hello world/, { exact: false });
-  expect(check).toBeInTheDocument();
+describe("Greeting component", () => {
+  test("checks for the hello world", () => {
+    // arrange
+    render(<Greeting />);
+    // acts
+    // => noting to do
+    // asserts
+    const check = screen.getByText(/Hello world/, { exact: false });
+    expect(check).toBeInTheDocument();
+  });
 });
