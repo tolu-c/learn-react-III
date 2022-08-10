a28cb20def896c1cafea138c52d93e5cd8a22b92
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-08-10 10:37:10 +0000

    testing async code - II

--- testing-react/src/components/Async.test.js
+++ testing-react/src/components/Async.test.js
@@ -2,10 +2,10 @@ import { render, screen } from "@testing-library/react";
 import Async from "./Async";
 
 describe("Async component", () => {
-  test("checks if post renders correctly", () => {
+  test("checks if post renders correctly", async () => {
     render(<Async />);
 
-    const check = screen.getAllByRole("listitem");
+    const check = await screen.findAllByRole("listitem");
     expect(check).not.toHaveLength(0);
   });
 });
