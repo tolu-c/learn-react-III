bb38bbfaf09759cbd0b0105d706b63d04f37c55a
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-08-14 22:47:54 +0000

    working with refs

--- react-ts/src/components/NewTodo.tsx
+++ react-ts/src/components/NewTodo.tsx
@@ -1,18 +1,29 @@
+import { useRef } from "react";
 
 
 const NewTodo = () => {
+  const titleRef = useRef<HTMLInputElement>(null)
+
   const todoSubmitHandler = (event: React.FormEvent) => {
     event.preventDefault();
+
+    const enteredTitle = titleRef.current!.value
+    
+    if (enteredTitle.trim().length === 0) {
+      return
+    }
+      
+    }
     // const todoText = (event.target as HTMLFormElement).elements.todoText.value;
     // if (todoText) {
     //   dispatch(addTodo(todoText));
     // }
-  };
+  // };
 
   return (
     <form onSubmit={todoSubmitHandler}>
       <label htmlFor="title">Title</label>
-      <input type="text" id="title" />
+      <input type="text" id="title" ref={titleRef} />
       <button type="submit">Add Todo</button>
     </form>
   );
